89e2ee6fdb8225d335e6f6f898096c1500e63054
omagara <omagara@oocl.com>
2021-08-03 23:35:12 +0800

    [POS-Machine][Ramon]feat:added function for getItemsInfo

--- PDCA.md
+++ PDCA.md
@@ -1,1 +1,5 @@
-**Function name:**getItemsInfo
+Function name:getItemsInfo
+P: 3 mins.
+D: 5 mins. 21s
+C: I had a hard time dealing with the List interface
+A: Familiarize more on the java collections framework
--- src/main/java/pos/machine/PosMachine.java
+++ src/main/java/pos/machine/PosMachine.java
@@ -6,4 +6,11 @@ public class PosMachine {
     public String printReceipt(List<String> barcodes) {
         return null;
     }
+
+    public static List<ItemInfo> getItemsInfo (){
+        ItemDataLoader items = new ItemDataLoader();
+        List<ItemInfo> itemsInfo= items.loadAllItemInfos();
+
+        return itemsInfo;
+    }
 }
