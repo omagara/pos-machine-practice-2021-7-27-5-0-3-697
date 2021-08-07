e5ee1e8afec4d09461056edd273058a2217e7d0a
omagara <omagara@oocl.com>
2021-08-07 11:02:44 +0800

    [POS-Machine][Ramon]refactor:make the return value of getItemsInfo inline

--- src/main/java/pos/machine/PosMachine.java
+++ src/main/java/pos/machine/PosMachine.java
@@ -8,55 +8,51 @@ public class PosMachine {
         return null;
     }
 
-    public List<ItemInfo> getItemsInfo (){
-        ItemDataLoader items = new ItemDataLoader();
-        List<ItemInfo> itemsInfo= items.loadAllItemInfos();
-
-        return itemsInfo;
+    public List<ItemInfo> getItemsInfo() {
+           return ItemDataLoader.loadAllItemInfos();
     }
 
-    public List<Item> convertToItems (List<String> barcodes){
+    public List<Item> convertToItems(List<String> barcodes) {
         List<ItemInfo> itemsInfo = getItemsInfo();
         List<Item> allItems = new ArrayList<>();
         int quantity;
-        int subTotal=0;
-        for(ItemInfo itemInfo: itemsInfo){
-            for(String barcode: barcodes){
-                if(barcode.equals(itemInfo.getBarcode())){
-                    quantity= Collections.frequency(barcodes, barcode);
-                    Item newItem = new Item(itemInfo.getName(), quantity, itemInfo.getPrice(),subTotal);
+        int subTotal = 0;
+        for (ItemInfo itemInfo : itemsInfo) {
+            for (String barcode : barcodes) {
+                if (barcode.equals(itemInfo.getBarcode())) {
+                    quantity = Collections.frequency(barcodes, barcode);
+                    Item newItem = new Item(itemInfo.getName(), quantity, itemInfo.getPrice(), subTotal);
                     allItems.add(newItem);
                 }
             }
         }
-        List<Item> purchasedItems = new ArrayList<>(new HashSet<>(allItems));;
+        List<Item> purchasedItems = new ArrayList<>(new HashSet<>(allItems));
+        ;
         return purchasedItems;
     }
 
-    public List<Item> calculateSubtotal (List<Item> purchasedItems){
+    public List<Item> calculateSubtotal(List<Item> purchasedItems) {
         List<Item> itemsWithSubtotal = new ArrayList<>();
-        for(Item itemInfo: purchasedItems){
-            int subTotal = itemInfo.getUnitPrice()* itemInfo.getQuantity();
-            Item updatedItem = new Item(itemInfo.getName(), itemInfo.getQuantity(), itemInfo.getUnitPrice(),subTotal);
+        for (Item itemInfo : purchasedItems) {
+            int subTotal = itemInfo.getUnitPrice() * itemInfo.getQuantity();
+            Item updatedItem = new Item(itemInfo.getName(), itemInfo.getQuantity(), itemInfo.getUnitPrice(), subTotal);
             itemsWithSubtotal.add(updatedItem);
 
         }
         return itemsWithSubtotal;
     }
 
-    public Receipt calculateTotalCost (List<Item> itemsWithSubtotal){
-        int totalCost=0;
-        for (Item itemInfo: itemsWithSubtotal){
+    public Receipt calculateTotalCost(List<Item> itemsWithSubtotal) {
+        int totalCost = 0;
+        for (Item itemInfo : itemsWithSubtotal) {
             totalCost += itemInfo.getSubTotal();
         }
-        Receipt receipt = new Receipt(totalCost,itemsWithSubtotal);
-        return receipt;
+        return new Receipt(totalCost, itemsWithSubtotal);
     }
 
-    public Receipt calculateReceipt (List<Item> purchasedItems){
+    public Receipt calculateReceipt(List<Item> purchasedItems) {
         List<Item> itemsWithSubtotal = calculateSubtotal(purchasedItems);
-        Receipt itemsWithTotalCost = calculateTotalCost(itemsWithSubtotal);
-        Receipt receipt = itemsWithTotalCost;
+        Receipt receipt = calculateTotalCost(itemsWithSubtotal);
 
         return receipt;
     }
